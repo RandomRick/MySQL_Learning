d8c9424140ed2e62b0cf2b837269c5c196c4d7a7
Rick Hellicar <rick.hellicar@outlook.com>
2020-10-03 21:13:10 +0100

    Basic functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbHostname = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDatabase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(553, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(774, 390);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tbHostname
+            // 
+            this.tbHostname.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHostname.Location = new System.Drawing.Point(12, 12);
+            this.tbHostname.Name = "tbHostname";
+            this.tbHostname.Size = new System.Drawing.Size(132, 26);
+            this.tbHostname.TabIndex = 2;
+            this.tbHostname.Text = "192.168.1.187";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUser.Location = new System.Drawing.Point(12, 41);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(132, 26);
+            this.tbUser.TabIndex = 2;
+            this.tbUser.Text = "RefcoUser";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(254, 12);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(132, 26);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.Text = "Refco123";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(150, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "user name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(392, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "password";
+            // 
+            // tbDatabase
+            // 
+            this.tbDatabase.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDatabase.Location = new System.Drawing.Point(254, 45);
+            this.tbDatabase.Name = "tbDatabase";
+            this.tbDatabase.Size = new System.Drawing.Size(132, 26);
+            this.tbDatabase.TabIndex = 2;
+            this.tbDatabase.Text = "rick_sandbox";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(392, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "database";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInsert.Enabled = false;
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(278, 501);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(233, 51);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRead.Enabled = false;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(15, 501);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(233, 51);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbDatabase);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbHostname);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbHostname;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDatabase;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnRead;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient; // RJH
 
 
 namespace MySQL_Learning
@@ -22,5 +24,124 @@ namespace MySQL_Learning
         {
 
         }
+
+
+    }
+}
+
+// all my stuff here
+namespace MySQL_Learning
+{
+    public partial class Form1 : Form
+    {
+        private string myConnectionString;
+        private MySql.Data.MySqlClient.MySqlConnection conn;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // preliminaries
+            (sender as Button).Enabled = false;
+            textBox1.AppendText("Connecting" + Environment.NewLine);
+
+            // build the connection string
+            myConnectionString =
+                string.Format("server={0};uid={1};pwd={2};database={3}",
+                tbHostname.Text,
+                tbUser.Text,
+                tbPassword.Text,
+                tbDatabase.Text
+                );
+#if DEBUG
+            textBox1.AppendText("DEBUG: ");
+            textBox1.AppendText("connection string : \""
+                + myConnectionString
+                + "\"" + Environment.NewLine);
+#endif
+
+            // attempt connection
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+#if DEBUG
+                textBox1.AppendText("DEBUG: successful connection" + Environment.NewLine);
+#endif
+                btnInsert.Enabled = true;
+                btnRead.Enabled = true;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        textBox1.AppendText("ERROR: ");
+                        textBox1.AppendText("Cannot connect to server.  Contact administrator" + Environment.NewLine);
+                        break;
+                    case 1045:
+                        textBox1.AppendText("ERROR: ");
+                        textBox1.AppendText("Invalid username/password, please try again" + Environment.NewLine);
+                        break;
+                    default:
+                        textBox1.AppendText("ERROR: ");
+                        textBox1.AppendText(ex.Message);
+                        break;
+                }
+            }
+
+            // conclusion
+            (sender as Button).Enabled = true;
+        }
+
+
+
+
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // https://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-sql-command.html
+            //string sql = @"SELECT * FROM et_pump_500hr.channel_4_flowrate;";
+            string sql = @"SELECT * FROM channel_4_flowrate;";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    string report = String.Format("{0} -- {1} -- {2} -- {3}",
+                        rdr[0], rdr[1], rdr[2], rdr[3]
+                        ) + Environment.NewLine;
+                    textBox1.AppendText(report);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText("ERROR: ");
+                textBox1.AppendText(ex.Message);
+            }
+        }
+
+
+
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = @"INSERT INTO channel_4_flowrate (`ID`, `dt`, `cycles`, `flow`) VALUES ('3', '2020-10-03', '99', '2.236067')";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText("ERROR: ");
+                textBox1.AppendText(ex.Message);
+            }
+
+        }
+
     }
 }
